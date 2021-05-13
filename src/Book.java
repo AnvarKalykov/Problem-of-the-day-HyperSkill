1341fe8cf83db967d19086113bcd150064ead0e0
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-05-13 23:51:11 +0600

    13052021

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+//Write a class named Book. It must have three fields: a string field title, an int field yearOfPublishing,
+//        an array of strings authors and a constructor to initialize these fields.
+//        The order of parameters in the constructor must be the same as presented above.
+
+class Book {
+    String title;
+    int yearOfPublishing;
+    String[] authors;
+
+    public Book(String title, int yearOfPublishing, String[] authors) {
+        this.title = title;
+        this.yearOfPublishing = yearOfPublishing;
+        this.authors = authors;
+    }
+}
