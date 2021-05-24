31cce3cc116fd4232f7553cc3906e4432361f9e7
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-05-24 22:36:02 +0600

    24052021

---
+++ src/ArrayOperations.java
@@ -0,0 +1,28 @@
+/*
+You need to implement reverseElements method. It should reverse all elements in the rows of the twoDimArray as in the example below.
+
+Example:
+
+0 0 9 9              9 9 0 0
+1 2 3 4 will become: 4 3 2 1
+5 6 7 8              8 7 6 5
+
+It is guaranteed that twoDimArray has at least 1 row.
+
+P.S. You don't need to print anything in this task.
+
+
+ */
+
+class ArrayOperations {
+    public static void reverseElements(int[][] twoDimArray) {
+        for (int i = 0; i < twoDimArray.length; i++) {
+            int[] line = twoDimArray[i];
+            int[] newline = new int[line.length];
+            for (int j = line.length - 1; j >= 0; j--) {
+                newline[line.length - 1 - j] = line[j];
+            }
+            twoDimArray[i] = newline;
+        }
+    }
+}
