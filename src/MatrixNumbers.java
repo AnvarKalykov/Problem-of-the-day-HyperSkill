4d5a05f8fce3abd371c8b1fb998aaa5b2f424a23
Anvar Kalykov <kalykov.anvar@gmail.com>
2021-05-29 21:46:59 +0600

    29052021

---
+++ src/MatrixNumbers.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MatrixNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[][] array = new int[n][n];
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = Math.abs(j + i);
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
+}
